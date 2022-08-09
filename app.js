7566d46bc3756c366c906fd79513e3fca5f04f0c
AbdallahMosa <abdallahmosa95@yahoo.com>
2022-08-09 21:36:36 +0300

    task 12

--- app.js
+++ app.js
@@ -28,46 +28,65 @@ function Mneu(foodName,typeOfFood,price){
         let price = event.target.price.value;
 
         const newMenu = new Mneu(name,typeOfFood,price)
-        newMenu.render();
+        // newMenu.render();
+        setData(allMneu)
+
+      
     } 
-    Mneu.prototype.render =  function(){
+  function setData (data){
+            let dataString = JSON.stringify(data)
+            localStorage.setItem("storageMenu",dataString)
 
-
-        let tr1 = document.createElement("tr");
-        table.appendChild(tr1);
-        let td1 = document.createElement("td")
-        td1.textContent = this.foodId();
-        tr1.appendChild(td1);
-        let td2 = document.createElement("td")
-        td2.textContent = this.foodName;
-        tr1.appendChild(td2);
-        let td3 = document.createElement("td")
-        td3.textContent = this.typeOfFood;
-        tr1.appendChild(td3);
-        let td4= document.createElement("td")
-        td4.textContent = this.price;
-        tr1.appendChild(td4);
-        // tr1.classList.add('row')
-
-        // for (let i = 0; i < allMneu.length; i++) {
-        //    const id1 = document.getElementById('id1')
-        //    const name1 = document.getElementById('name1')
-        //    const type1 = document.getElementById('type1')
-        //    const price1 = document.getElementById('price1')
-        //    id1.textContent= `${this.foodId()}`
-        //    name1.textContent = `${this.foodName}`
-        //    type1.textContent = `${this.typeOfFood}`
-        //    price1.textContent= `${this.price}`
-        //    allMneu[i].render
         }
-    
-   
-        
-    //  for (let i = 0; i < allMneu.length; i++) {
-    //    allMneu[i].render
-        
-    //  }
+    function getData (){
+        let returnData = localStorage.getItem("storageMenu") 
+        let arrayData = JSON.parse(returnData)
+        if (arrayData != null) {
+            
+       
+        for (let i = 0; i < arrayData.length; i++) {
+           new Mneu(
+            arrayData[i].foodId,
+            arrayData[i].foodName,
+            arrayData[i].typeOfFood,
+            arrayData[i].price
+            );
+            
+        } }}
+//     }
+//     Mneu.prototype.render =  function(){
 
+
+//         let tr1 = document.createElement("tr");
+//         table.appendChild(tr1);
+//         let td1 = document.createElement("td")
+//         td1.textContent = this.foodId();
+//         tr1.appendChild(td1);
+//         let td2 = document.createElement("td")
+//         td2.textContent = this.foodName;
+//         tr1.appendChild(td2);
+//         let td3 = document.createElement("td")
+//         td3.textContent = this.typeOfFood;
+//         tr1.appendChild(td3);
+//         let td4= document.createElement("td")
+//         td4.textContent = this.price;
+//         tr1.appendChild(td4);
+
+//     }
+// getData()
+// for (let i = 0; i < allMneu.length; i++) {
+//     allMneu[i].render();
+    
+// }
+
+
+
+
+
+
+
+
+        
 
      
    
